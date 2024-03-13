909f6fe4f406dfea217d7d4b38761d319766e533
wSwanepoel <wjaco.swanepoel@gmail.com>
2024-03-13 16:05:18 +1100

    added markdownit for in function markdown rendering

--- src/utils/commands/api_commands.jsx
+++ src/utils/commands/api_commands.jsx
@@ -1,15 +1,20 @@
 import { Fragment } from "vue/jsx-runtime";
-
+import markdownit from 'markdown-it';
 
 export const readme = async () => {
   const data = await $fetch('/api/getReadme');
-
+  const md = markdownit('commonmark', {
+    linkify: true,
+    typographer: true,
+  });
+  const result = md.render(data);
   return (
     <Fragment>
       <div className="whitespace-pre-wrap">
         <span>{`Opening GitHub README...`}</span>
         <br />
-        ${data}
+        <div innerHTML={result}>
+        </div>
       </div>
     </Fragment>
   );
