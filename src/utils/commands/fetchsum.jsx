1763b7e252d33477276d9af91b856fc20d13accb
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2024-03-22 15:26:49 +1100

    modified ascii art for fetchsum to use standard characters so that is looks better on mobile

--- src/utils/commands/fetchsum.jsx
+++ src/utils/commands/fetchsum.jsx
@@ -3,43 +3,58 @@ import { Fragment } from "vue/jsx-runtime";
 const fetchsum = async () => {
   const config = await $fetch('/api/getRuntimeConfig').catch(err =>
     import('../config.json'));
+  //   {`
+  //   |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+  //   | |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾| |
+  //   | |       Portfolio       | |
+  //   | |       Terminal        | |
+  //   | |       >_              | |
+  //   | |_______________________| |
+  //   |___________________________|
+  //   ____\\_____   []    _____/_____,
+  //  /        /___________\\        /|
+  // /_____________________________/ |
+  // |  _ _ _            [-------] | |
+  // |  o o o            [-------] | /
+  // |_____________________________|/
+  //   _______________________
+  //  //̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅- //
+  // / /̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-//
+  // ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾`}
   return (
     <Fragment>
-      <div className='flex flex-col items-start'>
+      <div className='flex flex-col items-start w-full'>
         {`fetchsum: summary display`}
-        <div className='flex flex-row flex-wrap sm:flex-nowrap max-w-full'>
+        <div className='flex flex-row flex-wrap sm:flex-nowrap justify-start w-full'>
           <div
-            className="whitespace-pre-wrap m-auto sm:mx-0 flex-grow-1"
+            className="m-auto sm:mx-2 flex-grow-2"
             style={{
               // gridTemplateRows: 'minmax(1rem, 10vmin) 1fr 5rem'
             }}>
             <pre
-              className=' leading-[normal]'
-              style={{
-                fontSize: "max(0.1rem, 2.5vmin)",
-                lineHeight: 'normal',
-              }}
+              className='h-max w-max text-[3vw] md:text-md lg:text-xl leading-[normal] md:leading-[normal] lg:leading-[normal] '
             >{`
-      |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
-      | |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾| |
-      | |       Portfolio       | |
-      | |       Terminal        | |
-      | |       >_              | |
-      | |_______________________| |
-      |___________________________|
-      ____\\_____   []    _____/_____,
-     /        /___________\\        /|
-    /_____________________________/ |
-    |  _ _ _            [-------] | | 
-    |  o o o            [-------] | /
-    |_____________________________|/
-      _______________________
-     //̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅- //
-    / /̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-/̅-//
-    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾`}</pre>
+   ___________________________
+  |  _______________________  |
+  | |                       | |
+  | |      Portfolio        | |
+  | |      Terminal         | |
+  | |      >_               | |
+  | |_______________________| |
+  |___________________________|
+  _____\\___    []    ___/_____
+ /        /__________\\       /|
+/___________________________/ |
+|  _ _ _                    | |
+|  o o o          [-------] | /
+|___________________________|/
+   ________________________
+  //`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/ /
+ / /`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`/`}<a style="text-decoration: underline">-</a>{`//
+/_______________________/`}</pre>
           </div>
 
-          <div className="whitespace-pre-wrap w-full mx-auto md:mx-0 my-4 sm:my-auto flex-grow-1">
+          <div className="whitespace-pre-wrap w-full sm:w-auto mx-auto md:mx-4 my-4 sm:my-auto flex-grow-1">
             <ul className="">
               <li className="min-w-full border-y-light-foreground : dark:border-y-dark-foreground border-solid border-y-2 py-2 my-4 list-none" > ABOUT</li>
               <li className="pl-4">{config.NAME}</li>
@@ -55,6 +70,7 @@ const fetchsum = async () => {
           </div>
         </div>
       </div>
+      <br />
     </Fragment>
   );
 };
